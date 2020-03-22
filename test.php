f2fdd932303c9e8dd48a254034cdea671ad01e3f
yanwei <18735386269@163.com>
2020-03-22 14:13:16 +0800

    test login

--- test.php
+++ test.php
@@ -1,2 +1,9 @@
 <?php
- echo 'Hello world';
+
+$code = $_POST['code'];
+$appid = "wx38770a80299936aa";
+$secret = "9fda945e79cc97f26c0dd8ff11770f3f";
+
+$url = "https://api.weixin.qq.com/sns/jscode2session?appid={$appid}&secret={$secret}&js_code={$code}&grant_type=authorization_code";
+
+echo $url;
