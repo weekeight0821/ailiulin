b206d8363ff6f13be11a5df3ac7d48236a07ca7c
yanwei <18735386269@163.com>
2020-03-22 14:17:22 +0800

    test

--- test.php
+++ test.php
@@ -13,5 +13,5 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $output = curl_exec($ch);
 curl_close($ch);
-$output = json_decode($output,true);
-return $output;
+echo '<pre>';
+print_r($output);
