ec99e015a47a868ec65a04eb8cb62f25c4467945
yanwei <18735386269@163.com>
2020-03-22 14:18:33 +0800

    test

--- test.php
+++ test.php
@@ -13,5 +13,4 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $output = curl_exec($ch);
 curl_close($ch);
-echo '<pre>';
-print_r($output);
+return $output;
