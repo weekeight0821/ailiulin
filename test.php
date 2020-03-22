609e0a0210acf6dea51f4a7a2933e3619f865a8e
yanwei <18735386269@163.com>
2020-03-22 14:16:14 +0800

    test

--- test.php
+++ test.php
@@ -6,4 +6,12 @@ $secret = "9fda945e79cc97f26c0dd8ff11770f3f";
 
 $url = "https://api.weixin.qq.com/sns/jscode2session?appid={$appid}&secret={$secret}&js_code={$code}&grant_type=authorization_code";
 
-echo $url;
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$output = curl_exec($ch);
+curl_close($ch);
+$output = json_decode($output,true);
+return $output;
